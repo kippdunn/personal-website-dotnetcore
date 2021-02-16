1a414c397e0e7258474249ad9abc573b2ec39aa7
Kipp Dunn <kipp.dunn@infernored.com>
2021-02-15 23:15:19 -0500

    Add auto generated dotnet core proj files

---
+++ kipp-personal-website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kipp_personal_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
